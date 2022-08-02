a8974e9d0a165410081287db8c08239032e56dae
Alex Silva <alexcssilva@gmail.com>
2022-08-01 21:39:40 -0300

    feat: added models/Car - update - Desafio 13

--- src/models/Car.ts
+++ src/models/Car.ts
@@ -3,9 +3,13 @@ import { ICar } from '../interfaces/ICar';
 import MongoModel from './MongoModel';
 
 const carMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
   doorsQty: Number,
   seatsQty: Number,
-});
+}, { versionKey: false });
 
 class Car extends MongoModel<ICar> {
   constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
