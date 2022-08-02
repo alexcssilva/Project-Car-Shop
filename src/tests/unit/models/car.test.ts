6b10e62b9e035de6ce82784b7296ea5864b19abd
Alex Silva <alexcssilva@gmail.com>
2022-08-02 19:43:58 -0300

    feat: update models/car.test

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import FrameModel from '../../../models/Car';
+import CarModel from '../../../models/Car';
 import { Model } from 'mongoose';
 import { carMock, carMockWithId} from '../../mocks/carMock';
 
 describe('Car Model', () => {
-	const carModel = new FrameModel();
+	const carModel = new CarModel();
 
 	before(() => {
 		sinon.stub(Model, 'create').resolves(carMockWithId);
